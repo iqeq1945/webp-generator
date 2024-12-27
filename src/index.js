0ea3909f6e0049c17ec0c5a10876a05bea129c69
Ungs <iqeq1945@gmail.com>
2024-12-28 02:05:44 +0900

    feat : beta generator

--- src/index.js
+++ src/index.js
@@ -6,18 +6,53 @@ const fs = require("fs");
 const app = express();
 const port = 3000;
 
-// 정적 파일 경로 설정
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/convert", async (req, res) => {
+app.get("/convert/:id", async (req, res) => {
+  const id = req.params.id;
   const filePath = path.join(__dirname, "..", "public", "image.png");
-
   if (!fs.existsSync(filePath)) {
     return res.status(404).send("파일을 찾을 수 없습니다.");
   }
 
+  const text = id || "하이하이";
+
   try {
-    const webpBuffer = await sharp(filePath).webp().toBuffer();
+    // 먼저 원본 이미지의 메타데이터를 가져옵니다
+    const metadata = await sharp(filePath).metadata();
+
+    // SVG 텍스트를 원본 이미지 크기에 맞게 생성
+    const maxLength = 4;
+    const lines = [];
+    for (let i = 0; i < text.length; i += maxLength) {
+      lines.push(text.slice(i, i + maxLength));
+    }
+    const svgText = `
+      <svg width="${metadata.width}" height="${metadata.height}">
+        <style>
+          .title { fill: #000; font-size: 64px; font-family: sans-serif; text-anchor: end; dominant-baseline: middle; }
+        </style>
+        ${lines
+          .map(
+            (line, index) =>
+              `<text x="${metadata.width * 0.7}" y="${
+                metadata.height / 2 + index * 50
+              }" class="title">${line}</text>`
+          )
+          .join("")}
+      </svg>
+    `;
+
+    const webpBuffer = await sharp(filePath)
+      .composite([
+        {
+          input: Buffer.from(svgText),
+          top: 0,
+          left: 0,
+        },
+      ])
+      .webp()
+      .toBuffer();
 
     res.set("Content-Type", "image/webp");
     res.send(webpBuffer);
